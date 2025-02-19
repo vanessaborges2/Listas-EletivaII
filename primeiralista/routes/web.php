142f40fc33dc7d59a1708db0b9862a3f0a2a5807
vanessaborges2 <vanessa.borges2@fatec.sp.gov.br>
2025-02-18 22:00:32 -0300

    Primeiro Exercício

--- primeiralista/routes/web.php
+++ primeiralista/routes/web.php
@@ -32,3 +32,15 @@ Route::post('/exer1resp', function(Request $request){
     $soma = $valor1 + $valor2;
     return view('exer1', compact('soma'));
 });
+
+Route::get('/ex1', function (){
+    return view('lista.ex1');
+});
+
+Route::post('/listaex1', function(Request $request){
+    $nota1 = floatval($request->input('nota1'));
+    $nota2 = floatval($request->input('nota2'));
+    $nota3 = floatval($request->input('nota3'));
+    $media = ($nota1 + $nota2 + $nota3) / 3;
+    return view('lista.ex1', compact('media'));
+});
